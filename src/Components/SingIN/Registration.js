ad0802adec367ed407d69dd1c77cd552a20d918d
DanilNefedov <ndanil2@ukr.net>
2022-11-17 16:23:22 +0200

    add new user in firebase

--- src/Components/SingIN/Registration.js
+++ src/Components/SingIN/Registration.js
@@ -1,6 +1,17 @@
 import { Form } from "./Form";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useDispatch } from "react-redux";
 
 export function Registration () {
+    const dispatch =useDispatch()
+
+    const handleRegister = ( email, password) =>{
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(console.log)
+            .catch(console.error)
+    }   
+
 
     const formProps = {
         nameForm:'Registration',
@@ -8,8 +19,8 @@ export function Registration () {
         link:'/login',
         nameLink:'Back to login'
     }
-
+    //console.log(handleLogin)formProps={formProps}
     return (
-        <Form formProps={formProps}></Form>
+        <Form  handleClick={handleRegister}></Form>
     )
 }
--- src/Components/SingIN/SingIn.js
+++ src/Components/SingIN/SingIn.js
@@ -1,7 +1,18 @@
 import { Form } from "./Form";
-
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import {setUser} from '../../store/authSlice'
 
 export function SingIn (){
+    const dispatch = useDispatch();
+
+
+    const handleLogin = (email, password) =>{
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+            .then(console.log)
+            .catch(console.error)
+    }
     
     const formProps = {
         nameForm:'Login',
@@ -10,6 +21,7 @@ export function SingIn (){
         nameLink:'Сreate an account'
     }
     return(
-        <Form formProps={formProps}></Form>
+        <Form ></Form>
+        // formProps={formProps} handleLogin={handleLogin}
     )
 }

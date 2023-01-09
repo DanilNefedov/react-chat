1b4f7cb22aa5558ef10a6b0f0d4f65b09b2c823f
DanilNefedov <ndanil2@ukr.net>
2023-01-09 17:47:15 +0200

    mrkp section profile and login

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -11,6 +11,11 @@ import { useEffect } from "react"
 import { Modal } from "../Modal/Modal"
 import { removeFrined } from "../../store/friendSlice"
 import { removeMessage } from "../../store/messagesSlice"
+import deleteAcc from '../../img/delete-acc.svg'
+import download from '../../img/download.svg'
+import edit from '../../img/edit.svg'
+import emailImg from '../../img/email.svg'
+import addPhoto from '../../img/add.svg'
 
 
 
@@ -157,9 +162,9 @@ export function Profile() {
             const data = doc.data()
             if (data) {
                 //console.log(data)
-                const name = data.name 
-                const photo = data.photoURL 
-                const email = data.email 
+                const name = data.name
+                const photo = data.photoURL
+                const email = data.email
                 dispatch(updateUser({ name, photo, email }))
             }
 
@@ -232,9 +237,9 @@ export function Profile() {
                 await deleteDoc(doc(db, "users", user.uid));
 
 
-                friend.map( async el => {
+                friend.map(async el => {
                     //console.log(el.id.reverse())
-                    await updateDoc(doc(db, 'chatsList',  user.uid), {
+                    await updateDoc(doc(db, 'chatsList', user.uid), {
                         [el.id]: deleteField(),
                         // lastMessage: deleteField(),
                         // userInfo: deleteField(),
@@ -243,26 +248,26 @@ export function Profile() {
                     });
                 })
 
-                
-                friend.map( async el => {
+
+                friend.map(async el => {
                     //console.log(el.id.reverse())
-                    await updateDoc(doc(db, 'chatsList',  el.friendId), {
+                    await updateDoc(doc(db, 'chatsList', el.friendId), {
                         //[el.id]: deleteField(),
                         [el.id + '.userInfo']: {
-                            photo:null,
-                            displayName:'Deleted',
+                            photo: null,
+                            displayName: 'Deleted',
                             acc: 'deleted'
                         },
-                        
+
                     });
                 })
 
-                
+
                 //await deleteDoc(doc(db, "chatsList", user.uid));
                 // friend.map( async el => {
                 //     await deleteDoc(doc(db, "chats", el.id));//not uid\ need combined id
                 // })
-                
+
 
                 signOut(auth).then(() => {
                     dispatch(removeUser())
@@ -281,7 +286,7 @@ export function Profile() {
             console.error(error)
         });
         // location.reload();
-        
+
 
     }
 
@@ -291,61 +296,108 @@ export function Profile() {
     return (
         <section className={classNames(style.profile, 'profile')}>
             <div className={classNames(style.container, 'container')}>
+                <div className={classNames(style.userInfo, "user-info")}>
+                    <div className={classNames(style.containerUserInfo, "container-userInfo")}>
+                        {user.photo ?
+                            <div className={classNames(style.photoSection, 'photo')}>
+                                <div className={classNames(style.userPhoto, "user-photo")}>
+                                    <img src={user.photo} alt="user" />
+                                </div>
+                                {/* <div className={classNames(style.editPhoto, "edit-photo")}>
+                                    <label className={style.downloadImg} htmlFor={style.loadPhoto}>Edit Photo</label>
+                                    <input id={style.loadPhoto} type="file" onChange={(e) => setPhoto(e.target.files[0])} accept='image/*, .png, .jpg, .web' />
+                                    <div className={classNames(style.deletePhoto, "delete-photo")}>
+                                        <button onClick={e => deletePhoto(e)} className={classNames(style.deleteBtn, "delete")}>Delete Photo</button>
+                                    </div>
+                                </div> */}
 
-                {user.photo ?
-                    <div className={classNames(style.photoSection, 'photo')}>
-                        <div className={classNames(style.userPhoto, "user-photo")}>
-                            <img src={user.photo} alt="user" />
-                        </div>
-                        <div className={classNames(style.editPhoto, "edit-photo")}>
-                            <label className={style.downloadImg} htmlFor={style.loadPhoto}>Edit Photo</label>
-                            <input id={style.loadPhoto} type="file" onChange={(e) => setPhoto(e.target.files[0])} accept='image/*, .png, .jpg, .web' />
-                            <div className={classNames(style.deletePhoto, "delete-photo")}>
-                                <button onClick={e => deletePhoto(e)} className={classNames(style.deleteBtn, "delete")}>Delete Photo</button>
                             </div>
-                        </div>
+                            :
+                            <div className={classNames(style.photoSection, 'photo')}>
+                                <div className={classNames(style.userPhoto, "user-photo")}>
+                                    <img src={img} alt="user" />
+                                </div>
+                                {/* <div className={classNames(style.editPhoto, "edit-photo")}>
+                                    <label className={style.downloadImg} htmlFor={style.loadPhoto}>Download Photo</label>
+                                    <input id={style.loadPhoto} type="file" onChange={(e) => setPhoto(e.target.files[0])} accept='image/*, .png, .jpg, .web' />
+                                </div> */}
+                            </div>
+                        }
+                        <div className={classNames(style.nameEmail, "name-email")}>
+                            <p className={classNames(style.nameUser, "name-user")}>
+                                <span className={classNames(style.editAbout, "head-name")}>Name:</span>
+                                <span className="header">{user.name}</span>
+                            </p>
+                            <p className={classNames(style.emailUser, "emailUser header")}>
+                                <span className={classNames(style.editAbout, "head-name")}> Email:</span>
+                                <span className="header">{user.email}</span>
+                            </p>
 
-                    </div>
-                    :
-                    <div className={classNames(style.photoSection, 'photo')}>
-                        <div className={classNames(style.userPhoto, "user-photo")}>
-                            <img src={img} alt="user" />
+                            <div className={classNames(style.containerBtn, "btn-delete")}>
+                                <img className={classNames(style.iconBtn)} src={deleteAcc} alt="delete" />
+                                <button onClick={(event) => {
+                                    event.preventDefault()
+                                    setDeleteUserState(true)
+                                }} 
+                                className={classNames(style.btnDelete)}>Delete Account</button>
+                            </div>
+                            
                         </div>
+                    </div>
+
+
+                </div>
+
+                <div className={classNames(style.editUserInfo, "edit-user-info")}>
+                    <div className={classNames(style.containerEditUser, "container")}>
+                        {user.photo? 
+                        <>
+                            <div className={classNames(style.editPhoto, "edit-photo")}>
+                                <img className={classNames(style.iconBtn)} src={download} alt="download" />
+                                <label className={style.downloadImg} htmlFor={style.loadPhoto}>Edit Photo</label>
+                                <input id={style.loadPhoto} type="file" onChange={(e) => setPhoto(e.target.files[0])} accept='image/*, .png, .jpg, .web' /> 
+                            </div> 
+                            <div className={classNames(style.deletePhoto, "delete-photo")}>
+                                <img className={classNames(style.iconBtn)} src={deleteAcc} alt="delete" />
+                                <button onClick={e => deletePhoto(e)} className={classNames(style.btnDelete, "delete")}>Delete Photo</button>
+                            </div>
+                        </>
+                        
+                        :
                         <div className={classNames(style.editPhoto, "edit-photo")}>
+                            <img className={classNames(style.iconBtn)} src={addPhoto} alt="add" />
                             <label className={style.downloadImg} htmlFor={style.loadPhoto}>Download Photo</label>
                             <input id={style.loadPhoto} type="file" onChange={(e) => setPhoto(e.target.files[0])} accept='image/*, .png, .jpg, .web' />
                         </div>
+                        }
+
+                        <div className={classNames(style.nameSection, 'name')}>
+                            <img className={classNames(style.iconBtn)} src={edit} alt="edit" />
+                            <span className={classNames(style.editField, 'head-name')}>Edit Name: </span>
+                            <input value={name} onChange={(e) => setName(e.target.value)} placeholder="enter name" type="text" className={classNames('edit-field', style.editName)} />
+                        </div>
+
+                        <div className={classNames(style.emailSection, "email")}>
+                            <img className={classNames(style.iconBtn)} src={emailImg} alt="edit" />
+                            <span className={classNames(style.editField, 'head-name')}>Edit Email: </span>
+                            <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="enter email" type="email" className={classNames('edit-field', style.editEmail)} />
+                        </div>
+                        <div className={classNames(style.updateSection, 'update')}>
+                            <button onClick={(event) => {
+                                email !== '' ? setActiveModal(true) : submiteUpdates(event)
+                            }} className={classNames(style.btnUpdate)}>Update</button>
+
+                            {/* <button onClick={(event) => {
+                                event.preventDefault()
+                                setDeleteUserState(true)
+                            }} className={classNames(style.btnDelete)}>Delete Account</button> */}
+                        </div>
                     </div>
-                }
 
-                <div className={classNames(style.nameSection, 'name')}>
-                    <p className={classNames(style.nameUser, "name-user")}>
-                        <span className={classNames(style.editAbout, "head-name")}>Name:</span>
-                        <span className="header">{user.name}</span>
-                    </p>
-                    <span className={classNames(style.editField, 'head-name')}>Edit Name: </span>
-                    <input value={name} onChange={(e) => setName(e.target.value)} placeholder="enter name" type="text" className={classNames('edit-field', style.editName)} />
-                </div>
-
-                <div className={classNames(style.emailSection, "email")}>
-                    <p className={classNames(style.emailUser, "emailUser header")}>
-                        <span className={classNames(style.editAbout, "head-name")}> Email:</span>
-                        <span className="header">{user.email}</span>
-                    </p>
-                    <span className={classNames(style.editField, 'head-name')}>Edit Email: </span>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="enter email" type="email" className={classNames('edit-field', style.editEmail)} />
-                </div>
-                <div className={classNames(style.updateSection, 'update')}>
-                    <button onClick={(event) => {
-                        email !== '' ? setActiveModal(true) : submiteUpdates(event)
-                    }} className={classNames(style.btnUpdate)}>Update</button>
-
-                    <button onClick={(event) => {
-                        event.preventDefault()
-                        setDeleteUserState(true)
-                    }} className={classNames(style.btnDelete)}>Delete Account</button>
                 </div>
             </div>
+
+
             <Modal deleteAccount={deleteAccount} setDeleteUserState={setDeleteUserState} deleteUserState={deleteUserState} submiteUpdates={submiteUpdates} activeModal={activeModal} setActiveModal={setActiveModal} passwodModal={passwodModal} setPasswordModal={setPasswordModal}></Modal>
         </section>
     )

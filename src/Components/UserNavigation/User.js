bd1857e5c96b3592860f94d84f7d652ac223390e
DanilNefedov <ndanil2@ukr.net>
2023-02-06 16:59:18 +0200

    test gitIgnore \ add link avatar to profile page

--- src/Components/UserNavigation/User.js
+++ src/Components/UserNavigation/User.js
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import img from '../../img/user-M.png'
 import classNames from "classnames";
 import style from './UserNavigation.module.css'
+import { Link } from 'react-router-dom';
 
 
 export function User() {
@@ -11,9 +12,9 @@ export function User() {
     return (
         <div className={style.user}>
             <p className={classNames(style.nameUser, 'user-name')}>{user.name ? user.name : 'Deleted'}</p>
-            <a href="#" >
+            <Link to="/profile" >
                 <img src={user.photo ? user.photo : img} alt="User"  className={classNames(style.photoUser, 'photo-user')}/>
-            </a>
+            </Link>
         </div>
     );
 
